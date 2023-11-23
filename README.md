2b2e3b0f04c499f775b20b23c1a6870c089f8563
Gustavo Messias <gustavo.messias96@gmail.com>
2023-11-23 10:02:49 -0300

    intial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# GHM.HttpResult
+
+## Install Package
+
+.NET CLI
+
+```sh
+dotnet add package GHM.HttpResult --version 1.0.0
+```
+
+Package Manager
+
+```sh
+NuGet\Install-Package GHM.HttpResult -Version 1.0.0
+```
+
+## Example
+
+## Star
+
+if you enjoy, don't forget the ⭐ and install the package 😊.
---
+++ src/GHM.HttpResult/Result/HttpError.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+
+namespace GHM.HttpResult.Result;
+
+public class NotFound : HttpError
+{
+    public NotFound(string title)
+        : base(title, HttpStatusCode.NotFound) { }
+}
+
+public class BadRequest : HttpError
+{
+    public BadRequest(string title)
+        : base(title, HttpStatusCode.BadRequest) { }
+}
+
+public class Conflict : HttpError
+{
+    public Conflict(string title)
+        : base(title, HttpStatusCode.Conflict) { }
+}
+
+public class Forbidden : HttpError
+{
+    public Forbidden(string title)
+        : base(title, HttpStatusCode.Forbidden) { }
+}
+
+public class Unauthorized : HttpError
+{
+    public Unauthorized(string title)
+        : base(title, HttpStatusCode.Unauthorized) { }
+}
