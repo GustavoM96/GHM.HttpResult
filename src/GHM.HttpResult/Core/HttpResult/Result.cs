3eb757fbb50fbadc1bda89f7577a2e45453abeb8
Gustavo Messias <gustavo.messias96@gmail.com>
2024-02-06 08:54:41 -0300

    refact: refact result

--- src/GHM.HttpResult/Core/HttpResult/Result.cs
+++ src/GHM.HttpResult/Core/HttpResult/Result.cs
@@ -58,21 +58,27 @@ public class Result
         StatusCode = SetErrorStatus(_errors);
     }
 
-    public static Result Ok => new(HttpStatusCode.OK);
-
     public static ValidationResult Validate(bool isError) => new(isError);
 
-    public ErrorResult ToErrorResult()
+    public ErrorResult ToError()
     {
         return Errors.Any() ? new(Message, StatusCode, Errors) : throw new ArgumentException("not found errors.");
     }
 
     public SuccessResult ToSuccessResult() => new(StatusCode);
 
-    public TResult Match<TResult>(Func<SuccessResult, TResult> onSuccess, Func<ErrorResult, TResult> onError)
+    public TResult Match<TResult>(Func<HttpStatusCode, TResult> onSuccess, Func<IEnumerable<Error>, TResult> onError)
     {
-        return IsValid ? onSuccess(ToSuccessResult()) : onError(ToErrorResult());
+        return IsValid ? onSuccess(StatusCode) : onError(Errors);
     }
+
+    public static Result Ok => new(HttpStatusCode.OK);
+
+    public static implicit operator Result(Error error) => new(error);
+
+    public static implicit operator Result(List<Error> errors) => new(errors);
+
+    public static implicit operator Result(Error[] errors) => new(errors);
 }
 
 public class Result<TData> : Result
@@ -95,10 +101,18 @@ public class Result<TData> : Result
     public Result(IEnumerable<Error> errors, HttpStatusCode statusCode)
         : base(errors, statusCode) { }
 
-    public SuccessResult<TData> ToSuccessDataResult() => new(Data, StatusCode);
+    public SuccessResult<TData> ToSuccess() => new(Data, StatusCode);
 
-    public TResult Match<TResult>(Func<SuccessResult<TData>, TResult> onSuccess, Func<ErrorResult, TResult> onError)
+    public TResult Match<TResult>(Func<TData, TResult> onSuccess, Func<IEnumerable<Error>, TResult> onError)
     {
-        return IsValid ? onSuccess(ToSuccessDataResult()) : onError(ToErrorResult());
+        return IsValid ? onSuccess(Data) : onError(Errors);
     }
+
+    public static implicit operator Result<TData>(TData data) => new(data, HttpStatusCode.OK);
+
+    public static implicit operator Result<TData>(Error error) => new(error);
+
+    public static implicit operator Result<TData>(List<Error> errors) => new(errors);
+
+    public static implicit operator Result<TData>(Error[] errors) => new(errors);
 }
